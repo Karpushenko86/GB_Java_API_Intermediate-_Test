0d5f587835df3e8aed62f652ce5042b5456ec3f8
Danya <saikirisoisor@mail.ru>
2024-08-08 23:13:37 +0500

    Update Laptop.java

--- src/main/java/com/geekbrains/Laptop.java
+++ src/main/java/com/geekbrains/Laptop.java
@@ -1,7 +1,70 @@
 package com.geekbrains;
 
-public class Laptop {
-    public static void main(String[] args) {
+import java.util.Objects;
 
+public class Laptop {
+    private String model;
+    private int ram;
+    private int hdd;
+    private String os;
+    private String color;
+
+    public Laptop(String model, int ram, int hdd, String os, String color) {
+        this.model = model;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.os = os;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "model='" + model + '\'' +
+                ", ram=" + ram +
+                ", hdd=" + hdd +
+                ", os='" + os + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Laptop laptop = (Laptop) o;
+
+        return ram == laptop.ram &&
+                hdd == laptop.hdd &&
+                Objects.equals(model, laptop.model) &&
+                Objects.equals(os, laptop.os) &&
+                Objects.equals(color, laptop.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, ram, hdd, os, color);
     }
 }
